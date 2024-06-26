8e696c41a38c964c4cc6334079aab4fa6ab8b47f
Aston Haycock <astonhaycocks@gmail.com>
2024-06-26 12:02:51 -0600

    added post request frontend

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:8080/cars";
+const URL = "http://localhost:8080";
 
 Vue.createApp({
   data(){
@@ -24,13 +24,58 @@ Vue.createApp({
       this.page = page
     },
     getCarList: async function(){
-      let response = await fetch(URL)
+      let response = await fetch(`${URL}/cars`)
       let data = await response.json();
       this.carList = data
-    }
-    // addCar: async function(page){
+    },
+    addCar: async function(){
+      try{
+        let myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+        let encodedData = "name=" + encodeURIComponent(this.car.name) + 
+        "exterior=" + encodeURIComponent(this.car.exColor) + 
+        "exterior=" + encodeURIComponent(this.car.exColor) +
+        "interior=" + encodeURIComponent(this.car.inColor) +
+        "price=" + encodeURIComponent(this.car.price) +
+        "mileage=" + encodeURIComponent(this.car.mileage) +
+        "transmission=" + encodeURIComponent(this.car.transmission) +
+        "drive=" + encodeURIComponent(this.car.driveType) +
+        "make=" + encodeURIComponent(this.car.make) +
+        "status=" + encodeURIComponent("in stock");
+
+        let requestOptions = {
+          
+          method: 'POST',
+          body: encodedData,
+          headers: myHeaders, 
+
+        }
+        let response = await fetch(`${URL}/cars`, requestOptions);
+  
+        if (response.ok) {
+          await this.getCarList();
+          this.car = {
+            name: "",
+            exColor: "",
+            inColor: "",
+            price: "",
+            mileage: "",
+            transmission: "",
+            driveType: "",
+            make: ""
+          };
+        } else {
+          alert('Error adding company');
+        }
+        
+
+      }
+      catch(error){
+
+        console.error('Error fetching company:', error);
+      }
       
-    // },
+    },
 
   },
   created: function(){

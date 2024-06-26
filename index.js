7726b276f0332ed95f9df6eb08d03f136157c015
cacutler <116305038+cacutler@users.noreply.github.com>
2024-06-26 11:40:28 -0600

    Switched to JSON

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 const app = express()
 app.use(express.static("public"))
 app.use(cors())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({limit: "10mb"}))
 app.get('/cars' , async function (req, res) { //retrieves all cars from the database
     try {
         let car = await model.Car.find()
